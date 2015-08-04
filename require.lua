2d9bf2a24c2f89722d2ab89a3aa4ba6d73c427f5
Pierre-Yves Gérardy <pygy79@gmail.com>
2015-08-05 00:55:47 +0200

    Remove useless parameter

--- require.lua
+++ require.lua
@@ -26,7 +26,7 @@ local package, p_loaded = package, package.loaded
 
 
 local sentinel do
-    local function errhandler(e) error("the require sentinel can't be indexed or updated") end
+    local function errhandler() error("the require sentinel can't be indexed or updated") end
     sentinel = newproxy and newproxy() or setmetatable({}, {__index = errhandler, __newindex = errhandler, __metatable = false})
 end
 
